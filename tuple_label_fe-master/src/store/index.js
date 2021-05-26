66f3f4450652e54a6e1f88f421a2badd5cb1fb7e
Ray <46347678+WO1353577267@users.noreply.github.com>
2021-05-26 11:26:36 +0800

    登录token验证+

--- tuple_label_fe-master/src/store/index.js
+++ tuple_label_fe-master/src/store/index.js
@@ -1,20 +1,25 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
- 
+
+const key ='Authorization'
 const store = new Vuex.Store({
  
   state: {
     // 存储token
-    Authorization: localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : ''
+    Authorization: localStorage.getItem(key) ? localStorage.getItem(key) : ''
   },
  
   mutations: {
     // 修改token，并将token存入localStorage
     changeLogin (state, user) {
       state.Authorization = user.Authorization;
-      localStorage.setItem('Authorization', user.Authorization);
-    }
+      localStorage.setItem(key, user.Authorization);
+    },
+    removeStorage(state){
+     state.token =null;
+     localStorage.removeItem(key)
+   }
   }
 });
  
